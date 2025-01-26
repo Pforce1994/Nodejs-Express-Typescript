9c09a1cdae9f4f1337a352517b55f9c0491ee052
Pforce1994 <pforce72@gmail.com>
2025-01-26 22:04:41 +0700

    version3

--- src/Router/myRouter.ts
+++ src/Router/myRouter.ts
@@ -1,34 +1,59 @@
 import express from "express";
-import path from 'path';
 
 const router = express.Router();
 
-const indexPage= path.join(__dirname, '../templates/index.html');
-const product1= path.join(__dirname, '../templates/product1.html');
-const product2= path.join(__dirname, '../templates/product2.html');
-const product3= path.join(__dirname, '../templates/product3.html');
-
-router.get('/index', (req: express.Request, res: express.Response) => {
-    res.status(200).sendFile(indexPage);
+router.get('/', (req: express.Request, res: express.Response) => {
+    const name:string = "GotDeveloper";
+    const age:number = 10;
+    const address:string = "<h3>Kanchanaburi</h3>";
+    res.render('index.ejs', {name: name, age: age, address: address});
+    
 })
 
-router.get('/product/:id', (req: express.Request, res: express.Response) => {
-    res.status(200);
-    // console.log(req.query.id);
-    console.log(req.params.id);
-    if (req.params.id === '1') {
-        res.status(200).sendFile(product1);
-        console.log(req.params.id);
-    } else if (req.params.id === '2') {
-        res.status(200).sendFile(product2);
-        console.log(req.params.id);
-    } else if (req.params.id === '3') {
-        res.status(200).sendFile(product3);
-        console.log(req.params.id);
-    } else {
-        res.redirect('/index');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const indexPage= path.join(__dirname, '../templates/index.html');
+// const product1= path.join(__dirname, '../templates/product1.html');
+// const product2= path.join(__dirname, '../templates/product2.html');
+// const product3= path.join(__dirname, '../templates/product3.html');
+
+
+// router.get('/index', (req: express.Request, res: express.Response) => {
+//     res.status(200).sendFile(indexPage);
+// })
+
+// router.get('/product/:id', (req: express.Request, res: express.Response) => {
+//     res.status(200);
+//     // console.log(req.query.id);
+//     console.log(req.params.id);
+//     if (req.params.id === '1') {
+//         res.status(200).sendFile(product1);
+//         console.log(req.params.id);
+//     } else if (req.params.id === '2') {
+//         res.status(200).sendFile(product2);
+//         console.log(req.params.id);
+//     } else if (req.params.id === '3') {
+//         res.status(200).sendFile(product3);
+//         console.log(req.params.id);
+//     } else {
+//         res.redirect('/index');
         
-    }
-})
+//     }
+// })
 
 export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import path from 'path';
-// import myRouter from './Router/myRouter';
+import router from './Router/myRouter';
 
 const app: express.Application = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(myRouter);
- 
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
